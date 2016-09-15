2ff32370141c88bc52877b11848d0ac19f256462
Ellie Dori <email": null>
2016-09-15 10:05:05 -0700

    Add full set of tests for hello siri

--- spec/hello_siri_spec.rb
+++ spec/hello_siri_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../hello_siri'
 require 'rspec'
+require 'date'
 
 
 ###################
@@ -15,7 +16,26 @@ describe "#hello_siri" do
   end
 
   context "when command and name arguments are valid" do
-    # Write your 5+ tests here
+    it "correctly interpolates name into response" do
+      expect(hello_siri("Hello","Ellie")).to eq "Hello Ellie."
+    end
+
+     it "returns today's date when asked" do
+      expect(hello_siri("What day is it?")).to eq Date.today
+    end
+
+    it "returns an integer when told person is feeling lucky" do
+      expect(hello_siri("I'm feeling lucky").class).to be Fixnum
+    end
+
+   it "gives a sassy response when asked a silly question" do
+      expect(hello_siri("How do I become an amazing developer?")).to eq "Google it!"
+    end
+
+   it "gives standard response when given any other input" do
+      expect(hello_siri("Do fish sleep?")).to eq "I don't understand. Should I call your emergency contact number?"
+    end
+
   end
 
 end
